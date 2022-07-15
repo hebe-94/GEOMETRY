2a2cae1b1ea2d9eda7c95eda7bbb1af63e0277f1
shshin <shshin@geotwo.com>
2022-07-15 11:17:25 +0900

    JSP Thymeleaf 동시 사용 설정

--- src/main/java/com/geo/semiproject/controller/pageController.java
+++ src/main/java/com/geo/semiproject/controller/pageController.java
@@ -1,6 +1,7 @@
 package com.geo.semiproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -9,6 +10,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class pageController {
     @RequestMapping(value = "",  method = RequestMethod.GET)
     public String start() {
-        return "index.html";
+        return "thymeleaf/index";
+    }
+
+    @GetMapping("/jsp")
+    public String test(){
+        return "/index";
     }
 }
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,10 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR" pageEncoding="EUC-KR" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="EUC-KR">
+    <title>Jsp page sample</title></head>
+<body>
+<h2>jsp body sample</h2>
+</body>
+</html>
